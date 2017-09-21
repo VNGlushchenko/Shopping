60a3775317e59172ea9913b7379b9e6b0a4b2f0a
Vladimir Glushchenko <v.glushchenko@gmail.com>
2017-09-22 00:18:08 +0300

    routing.js was improved

--- app/routing.js
+++ app/routing.js
@@ -10,7 +10,20 @@
       $stateProvider
         .state('shoppingList', {
           url: '/',
-          templateUrl: 'app/components/app-shopping-list.template.html'
+          templateUrl: 'app/components/app-shopping-list.template.html',
+          onExit: [
+            'ShoppingModel',
+            function(ShoppingModel) {
+              ShoppingModel.model.goodsCatalog.length = 0;
+              ShoppingModel.model.newProductName = '';
+              ShoppingModel.model.newProductErrorText = '';
+              //ShoppingModel.model.categoriesList.length = 0;
+              ShoppingModel.model.previousShoppingList.length = 0;
+              ShoppingModel.model.actualShoppingListLength = 0;
+              ShoppingModel.model.categoriesTotalCosts.length = 0;
+              ShoppingModel.model.shielduiGridRepository.length = 0;
+            }
+          ]
         })
         .state('shoppingStatistics', {
           url: '/statistics',
